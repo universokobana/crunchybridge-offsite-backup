46457d112b323aaefa5942a6d5ae986ff6dd9a97
Rafael Lima <contato@rafael.adm.br>
2023-03-15 20:47:58 -0300

    Updates the setup to a new file sctructure and make everythign work

--- setup.sh
+++ setup.sh
@@ -37,7 +37,7 @@ if [ "$CBOB_SLACK_CLI_TOKEN" == "" ]; then
   warning 'No Slack Token given, this feature will be disabled.'
 else
   if [[ -z "$CBOB_SLACK_CHANNEL" ]]; then
-    read -re -p 'Slack channel to send logs: ' -i '#backup_log' CBOB_SLACK_CHANNEL
+    read -re -p 'Slack channel to send logs: ' -i '#backup-log' CBOB_SLACK_CHANNEL
     if [ "$CBOB_SLACK_CHANNEL" == "" ]; then
       error 'A channel must be given'
     fi
@@ -51,47 +51,68 @@ if [[ -z "$CBOB_CRUNCHY_CLUSTERS" ]]; then
   fi
 fi
 
+if [[ -z "$CBOB_RETENTION_FULL" ]]; then
+  read -re -p 'How many full backups you would like to retain: ' -i '7' CBOB_RETENTION_FULL
+fi
+
 if [[ -z "$CBOB_BASE_PATH" ]]; then
-  read -re -p 'Path where Crunchy Bridge Offsite Backup data will reside: ' -i '/mnt/volume_pgbackrest_backups' CBOB_BASE_PATH
+  read -re -p 'Path where Crunchy Bridge Offsite Backup data will reside: ' -i '/mnt/volume_cbob' CBOB_BASE_PATH
   echo "Crunchy Bridge Offsite Backup repository path: $CBOB_BASE_PATH"
 fi
 
-# info "Creating admin user"
-# if id "admin" &>/dev/null; then
-#   info "  User admin already exists!"
-# else
-#   sudo useradd admin
-# fi
+info "Updating and upgrading packages"
+apt update && apt upgrade -y
+
+info "Installing dependencies"
+apt install software-properties-common apt-transport-https wget curl ca-certificates gnupg jq unzip sendemail -y
+curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+./aws/install --update
+rm -Rf ./aws awscliv2.zip
 
 info "Adding Postgresql repository"
-if [[ -z /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg ]]; then
+if [ ! -f /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg ]; then
   info "  Dowloading Postgresql GPG"
-  sudo apt install -y curl ca-certificates gnupg
-  curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
+  curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
+  # wget -O- https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | tee /usr/share/keyrings/postgresql.gpg
 fi
-if [[ $(cat /etc/apt/sources.list.d/pgdg.list |grep apt.postgresql.org) == "" ]]; then
-  info "  Adding repo on source list"
-  sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-  sudo apt update
+if [ ! -f /etc/apt/sources.list.d/pgdg.list ]; then
+  info "  Adding repo on sources.list"
+  sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+  # echo deb [arch=amd64,arm64,ppc64el signed-by=/usr/share/keyrings/postgresql.gpg] http://apt.postgresql.org/pub/repos/apt/ bullseye-pgdg main | tee /etc/apt/sources.list.d/postgresql.list
+  apt update
 fi
 
-info "Installing Packages"
-sudo apt install -y postgresql-15 postgresql-client-15 postgresql-15-pgaudit libdbd-pg-perl pgbackrest
+info "Installing main packages"
+apt install -y postgresql-15 postgresql-client-15 postgresql-15-pgaudit pgbackrest
 
-info "Installing pgbackrest_auto with the latest version"
-sudo wget -q https://raw.githubusercontent.com/universokobana/pgbackrest_auto/patched/pgbackrest_auto
-sudo mv pgbackrest_auto /usr/local/bin/pgbackrest_auto
-sudo chown postgres:postgres /usr/local/bin/pgbackrest_auto
-sudo chmod 750 /usr/local/bin/pgbackrest_auto
+info "Installing (patched version of) pgbackrest_auto"
+wget -q https://raw.githubusercontent.com/universokobana/pgbackrest_auto/patched/pgbackrest_auto
+mv pgbackrest_auto /usr/local/bin/pgbackrest_auto
+chown postgres:postgres /usr/local/bin/pgbackrest_auto
+chmod 750 /usr/local/bin/pgbackrest_auto
+
+info "Creating admin user with sudo privileges"
+if id "admin" &>/dev/null; then
+  info "  User admin already exists!"
+else
+  info "Choose the password for admin user"
+  adduser --gecos "" admin
+  usermod -aG sudo admin
+  cp -r ~/.ssh /home/admin
+  echo "export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8" >> /home/admin/.bashrc
+  chown -R admin:admin /home/admin
+fi
 
 info "Creating pgBackRest config files & directories"
-sudo mkdir -p -m 770 /var/log/pgbackrest
-sudo chown postgres:postgres /var/log/pgbackrest
-sudo mkdir -p /etc/pgbackrest
-sudo mkdir -p /etc/pgbackrest/conf.d
-sudo chown postgres:postgres -R /etc/pgbackrest
-sudo mkdir -p /tmp/pgbackrest
-sudo chown postgres:postgres -R /tmp/pgbackrest
+mkdir -p -m 770 /var/log/pgbackrest
+chown postgres:postgres /var/log/pgbackrest
+mkdir -p /etc/pgbackrest
+mkdir -p /etc/pgbackrest/conf.d
+chown postgres:postgres -R /etc/pgbackrest
+mkdir -p /tmp/pgbackrest
+chown postgres:postgres -R /tmp/pgbackrest
 
 info "Checking if repo_path is a external volume"
 if cat /etc/fstab | grep " $CBOB_BASE_PATH "; then
@@ -101,71 +122,82 @@ if cat /etc/fstab | grep " $CBOB_BASE_PATH "; then
   else
     info "  Mounting external volume"
     mkdir -p $CBOB_BASE_PATH
-    sudo mount $CBOB_BASE_PATH
+    mount $CBOB_BASE_PATH
   fi
 else
   info "  Path is not for external volume"
 fi
 
-info "Creating directory for Crunchy Bridge sync"
-sudo mkdir -p $CBOB_BASE_PATH/crunchybridge
-sudo chmod 750 $CBOB_BASE_PATH/crunchybridge
-sudo chown postgres:postgres -R $CBOB_BASE_PATH/crunchybridge
+info "Creating directory for sync (used by cbob_sync)"
+mkdir -p $CBOB_BASE_PATH/crunchybridge
+chmod 750 $CBOB_BASE_PATH/crunchybridge
+mkdir -p $CBOB_BASE_PATH/crunchybridge/archive
+chmod 750 $CBOB_BASE_PATH/crunchybridge/archive
+mkdir -p $CBOB_BASE_PATH/crunchybridge/backup
+chmod 750 $CBOB_BASE_PATH/crunchybridge/backup
+chown postgres:postgres -R $CBOB_BASE_PATH/crunchybridge
 
-info "Creating directory for restores (used by pgbackrest_auto)"
-sudo mkdir -p $CBOB_BASE_PATH/restores
-sudo chmod 750 $CBOB_BASE_PATH/restores
-sudo chown postgres:postgres -R $CBOB_BASE_PATH/restores
+info "Creating directory for restores (used by cbob_restore_check)"
+mkdir -p $CBOB_BASE_PATH/restores
+chmod 750 $CBOB_BASE_PATH/restores
+chown postgres:postgres $CBOB_BASE_PATH/restores
 
-info "Creating directory for postgresql data"
-sudo mkdir -p $CBOB_BASE_PATH/postgresql/15
-sudo chmod 750 $CBOB_BASE_PATH/postgresql/15
-sudo chown postgres:postgres -R $CBOB_BASE_PATH/postgresql
-
-info "Creating directory for pgbackrest configs"
-sudo mkdir -p $CBOB_BASE_PATH/pgbackrest
-sudo chmod 750 $CBOB_BASE_PATH/pgbackrest
-sudo chown postgres:postgres -R $CBOB_BASE_PATH/pgbackrest
+info "Creating directory for postgresql data (used by manual restores)"
+mkdir -p $CBOB_BASE_PATH/postgresql/15
+chmod 750 $CBOB_BASE_PATH/postgresql/15
+chown postgres:postgres -R $CBOB_BASE_PATH/postgresql
 
 info "Creating directory for CBOB logs"
+mkdir -p $CBOB_BASE_PATH/log
+chmod 750 $CBOB_BASE_PATH/log
+mkdir -p $CBOB_BASE_PATH/log/cbob
+chmod 750 $CBOB_BASE_PATH/log/cbob
+mkdir -p $CBOB_BASE_PATH/log/pgbackrest
+chmod 750 $CBOB_BASE_PATH/log/pgbackrest
+mkdir -p $CBOB_BASE_PATH/log/postgresql
+chmod 750 $CBOB_BASE_PATH/log/postgresql
+chown postgres:postgres -R $CBOB_BASE_PATH/log
 
-sudo mkdir -p $CBOB_BASE_PATH/log
-sudo chmod 750 $CBOB_BASE_PATH/log
-sudo mkdir -p $CBOB_BASE_PATH/log/cbob
-sudo chmod 750 $CBOB_BASE_PATH/log/cbob
-sudo mkdir -p $CBOB_BASE_PATH/log/pgbackrest
-sudo chmod 750 $CBOB_BASE_PATH/log/pgbackrest
-sudo mkdir -p $CBOB_BASE_PATH/log/postgresql
-sudo chmod 750 $CBOB_BASE_PATH/log/postgresql
-sudo chown postgres:postgres -R $CBOB_BASE_PATH/log
+# info "Adding Crunchy Bridge API Tokent to /etc/profile.d"
+# echo "export CRUNCHY_API_KEY=$CBOB_CRUNCHY_API_KEY" > /etc/profile.d/cbob_crunchybridge
 
-info "Adding Crunchy Bridge API Tokent to /etc/profile.d"
-sudo echo "export CRUNCHY_API_KEY=$CBOB_CRUNCHY_API_KEY" > /etc/profile.d/cbob_crunchybridge
+info "Adding Slack CLI Token to /etc/profile.d"
+rm -f /etc/profile.d/slack_cli.sh
+echo "export SLACK_CLI_TOKEN=$CBOB_SLACK_CLI_TOKEN" >> /etc/profile.d/slack_cli.sh
+echo "export SLACK_CHANNEL=$CBOB_SLACK_CHANNEL" >> /etc/profile.d/slack_cli.sh
+
+info "Updating environemnt variables"
+export SLACK_CLI_TOKEN=$CBOB_SLACK_CLI_TOKEN
+export SLACK_CHANNEL=$CBOB_SLACK_CHANNEL
 
 info "Installing scripts e config files"
-info "  Copying ./bin/cbob_sync to /usr/local/bin/cbob_sync"
-sudo cp ./bin/cbob_sync /usr/local/bin/cbob_sync
-sudo chown postgres:postgres /usr/local/bin/cbob_sync
+info "  Copying cbob_sync"
+cp ./bin/cbob_sync /usr/local/bin/cbob_sync_pg
+chown postgres:postgres /usr/local/bin/cbob_sync_pg
 
-info "  Copying ./bin/slack to /usr/local/bin/slack"
-sudo cp -n ./bin/slack /usr/local/bin/slack
-sudo chown postgres:postgres /usr/local/bin/slack
+info "  Copying slack-cli"
+cp -n ./bin/slack /usr/local/bin/slack
+chown postgres:postgres /usr/local/bin/slack
 
 info "  Creating config at /usr/local/etc/cb_offsite_backup"
 echo "CBOB_CRUNCHY_API_KEY=$CBOB_CRUNCHY_API_KEY
 CBOB_CRUNCHY_CLUSTERS=$CBOB_CRUNCHY_CLUSTERS
-CBOB_TARGET_PATH=$CBOB_BASE_PATH
+CBOB_TARGET_PATH=$CBOB_BASE_PATH/crunchybridge
 CBOB_LOG_PATH=$CBOB_BASE_PATH/log/cbob" > /usr/local/etc/cb_offsite_backup
 if [ -n "${CBOB_SLACK_CLI_TOKEN}" ]; then
   echo "CBOB_SLACK_CLI_TOKEN=$CBOB_SLACK_CLI_TOKEN
-  CBOB_SLACK_CHANNEL=$CBOB_SLACK_CHANNEL" >> /usr/local/etc/cb_offsite_backup
+CBOB_SLACK_CHANNEL=$CBOB_SLACK_CHANNEL" >> /usr/local/etc/cb_offsite_backup
 fi
 
-info "  Copying ./etc/logrotate.d/cb_offsite_backup to /etc/logrotate.d/"
-sudo cp -n ./etc/logrotate.d/cb_offsite_backup /etc/logrotate.d/
+info "  Configuring logrotate"
+cp -n ./etc/logrotate.d/cb_offsite_backup /etc/logrotate.d/
 
-info "  Linking /usr/local/bin/crunchybridge_offsite_backup to /etc/cron.daily"
-sudo ln -sf /usr/local/bin/crunchybridge_offsite_backup /etc/cron.daily/crunchybridge_offsite_backup
+info "Setting locale"
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+dpkg-reconfigure -fnoninteractive locales
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
 info "Stopping postgresql clusters"
 cbob_postgres_stop || true
@@ -175,7 +207,14 @@ TMP_PATH="/tmp/cbob-generated-configs"
 mkdir -p $TMP_PATH
 mkdir -p $TMP_PATH/bin
 
-echo "#!/bin/bash" > "$TMP_PATH/bin/cbob_cron"
+echo "#!/bin/bash" > "$TMP_PATH/bin/cbob_sync"
+echo "sudo -u postgres cbob_sync_pg"  >> "$TMP_PATH/bin/cbob_sync"
+
+echo "#!/bin/bash" > "$TMP_PATH/bin/cbob_sync_and_expire"
+echo "cbob_sync"  >> "$TMP_PATH/bin/cbob_sync_and_expire"
+echo "cbob_expire"  >> "$TMP_PATH/bin/cbob_sync_and_expire"
+
+echo "#!/bin/bash" > "$TMP_PATH/bin/cbob_restore_check"
 echo "#!/bin/bash" > "$TMP_PATH/bin/cbob_check"
 echo "#!/bin/bash" > "$TMP_PATH/bin/cbob_info"
 echo "#!/bin/bash" > "$TMP_PATH/bin/cbob_expire"
@@ -193,72 +232,68 @@ for CLUSTER_ID in "${CLUSTER_IDS[@]}"; do # access each element of array
   info "Processing cluster $CLUSTER_ID"
   CREDENTIALS_RESPONSE=$(curl -s -X POST "https://api.crunchybridge.com/clusters/$CLUSTER_ID/backup-tokens" -H "Authorization: Bearer $CBOB_CRUNCHY_API_KEY")
   STANZA=$(echo $CREDENTIALS_RESPONSE | jq -r '.stanza')
+  if [ "$STANZA" == "null" ]; then
+    error "Missing STANZA for CLUSTER $CLUSTER_ID. Response: $CREDENTIALS_RESPONSE"
+  fi
+
   info "  -> Stanza $STANZA"
 
   info "  Initializing database for cluster $CLUSTER_ID"
-  sudo -u postgres rm -rf $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID
+  rm -rf $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID
   sudo -u postgres /usr/lib/postgresql/15/bin/initdb -D $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID
 
-  info "  Updating postgresql.com cluster $CLUSTER_ID"
-  sudo mv $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID/postgresql.conf $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID/postgresql.default.conf
-
-  sudo cp ./etc/postgresql.template.conf $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID/postgresql.conf
+  info "  Updating postgresql.conf cluster $CLUSTER_ID"
+  mv $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID/postgresql.conf $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID/postgresql.default.conf
+  cp ./etc/postgresql.template.conf $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID/postgresql.conf
   sed -i -e "s/{{stanza}}/$STANZA/g" $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID/postgresql.conf
   sed -i -e "s/{{port}}/$port_counter/g" $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID/postgresql.conf
-  sudo chown postgres:postgres $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID/postgresql.conf
+  chown postgres:postgres $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID/postgresql.conf
 
   info "  Adding cluster $CLUSTER_ID to script files"
   echo "sudo -u postgres /usr/lib/postgresql/15/bin/pg_ctl -D $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID -l $CBOB_BASE_PATH/log/postgresql/postgresql-15-$CLUSTER_ID.log start" >> "$TMP_PATH/bin/cbob_postgres_start"
   echo "sudo -u postgres /usr/lib/postgresql/15/bin/pg_ctl -D $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID -l $CBOB_BASE_PATH/log/postgresql/postgresql-15-$CLUSTER_ID.log stop" >> "$TMP_PATH/bin/cbob_postgres_stop"
   echo "sudo -u postgres /usr/lib/postgresql/15/bin/pg_ctl -D $CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID -l $CBOB_BASE_PATH/log/postgresql/postgresql-15-$CLUSTER_ID.log restart" >> "$TMP_PATH/bin/cbob_postgres_restart"
-  echo "pgbackrest_auto --from=$STANZA --to=$CBOB_BASE_PATH/restores/$STANZA --checkdb --clear --config=$CBOB_BASE_PATH/pgbackrest/pgbackrest.$STANZA.full.conf" >> "$TMP_PATH/bin/cbob_cron"
-  echo "sudo -u postgres pgbackrest --stanza=$STANZA check --config=$CBOB_BASE_PATH/pgbackrest/pgbackrest.$STANZA.full.conf"  >> "$TMP_PATH/bin/cbob_check"
-  echo "sudo -u postgres pgbackrest --stanza=$STANZA info --config=$CBOB_BASE_PATH/pgbackrest/pgbackrest.$STANZA.full.conf"  >> "$TMP_PATH/bin/cbob_info"
-  echo "sudo -u postgres pgbackrest --stanza=$STANZA expire --config=$CBOB_BASE_PATH/pgbackrest/pgbackrest.$STANZA.full.conf"  >> "$TMP_PATH/bin/cbob_expire"
+  echo "sudo -u postgres pgbackrest_auto --from=$STANZA --to=$CBOB_BASE_PATH/restores/$STANZA --checkdb --clear --report --config=/etc/pgbackrest/pgbackrest.conf" >> "$TMP_PATH/bin/cbob_restore_check"
+  echo "sudo -u postgres pgbackrest --stanza=$STANZA check"  >> "$TMP_PATH/bin/cbob_check"
+  echo "sudo -u postgres pgbackrest --stanza=$STANZA info"  >> "$TMP_PATH/bin/cbob_info"
+  echo "sudo -u postgres pgbackrest --stanza=$STANZA expire"  >> "$TMP_PATH/bin/cbob_expire"
 
   echo "[global]
 start-fast=y
 log-level-file=detail
 log-path=$CBOB_BASE_PATH/log/pgbackrest
-repo1-retention-full=7
+repo1-retention-full=$CBOB_RETENTION_FULL
+repo1-path=$CBOB_BASE_PATH/crunchybridge
 " > "$TMP_PATH/pgbackrest.conf"
 
   echo "[$STANZA]
 pg1-path=$CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID
 pg1-port=$port_counter
-repo-path=$CBOB_BASE_PATH/crunchybridge/$CLUSTER_ID/$STANZA
-" > "$TMP_PATH/pgbackrest.$STANZA.part.conf"
-
-  echo "[$STANZA]
-pg1-path=$CBOB_BASE_PATH/postgresql/15/$CLUSTER_ID
-pg1-port=$port_counter
-
-[global]
-start-fast=y
-log-level-file=detail
-log-path=$CBOB_BASE_PATH/log/pgbackrest
-repo1-retention-full=7
-repo1-path=$CBOB_BASE_PATH/crunchybridge/$CLUSTER_ID/$STANZA
-" > "$TMP_PATH/pgbackrest.$STANZA.full.conf"
+" >> "$TMP_PATH/pgbackrest.conf"
 
   ((port_counter++))
 done
 
 info "Installing generated files"
-sudo chown postgres:postgres $TMP_PATH/*
-sudo chmod +x $TMP_PATH/bin/*
-sudo cp $TMP_PATH/bin/* /usr/local/bin/
-sudo cp $TMP_PATH/pgbackrest.conf /etc/pgbackrest/pgbackrest.conf
-sudo cp $TMP_PATH/pgbackrest.*.part.conf /etc/pgbackrest/conf.d/
-sudo cp $TMP_PATH/pgbackrest.*.full.conf $CBOB_BASE_PATH/pgbackrest/
+chown postgres:postgres $TMP_PATH/*
+chmod +x $TMP_PATH/bin/*
+cp $TMP_PATH/bin/* /usr/local/bin/
+cp $TMP_PATH/pgbackrest.conf /etc/pgbackrest/pgbackrest.conf
+
+info "Adding automatic sync to cron"
+rm -f /etc/cron.d/cbob_sync_and_expire
+echo "# Crunchy Data Offsite Backup" >> /etc/cron.d/cbob_sync_and_expire
+echo "00 06 * * * root /usr/local/bin/cbob_sync_and_expire" >> /etc/cron.d/cbob_sync_and_expire
 
 info "Adding automatic restore to cron"
-rm -f /etc/cron.d/cbob
-sudo echo "# Crunchy Data Offsite Backup" >> /etc/cron.d/cbob
-sudo echo "00 16 * * * postgres /usr/local/bin/cbob_cron" >> /etc/cron.d/cbob
+rm -f /etc/cron.d/cbob_restore_check
+echo "# Crunchy Data Offsite Backup" >> /etc/cron.d/cbob_restore_check
+echo "00 18 * * * root /usr/local/bin/cbob_restore_check" >> /etc/cron.d/cbob_restore_check
 
 info "Removing tmp files"
-rm -f $TMP_PATH/cbob_cron
+rm -f $TMP_PATH/cbob_restore_check
+rm -f $TMP_PATH/cbob_sync
+rm -f $TMP_PATH/cbob_sync_and_expire
 rm -f $TMP_PATH/cbob_check
 rm -f $TMP_PATH/cbob_info
 rm -f $TMP_PATH/cbob_expire
@@ -267,11 +302,10 @@ rm -f $TMP_PATH/cbob_postgres_stop
 rm -f $TMP_PATH/cbob_postgres_restart
 rm -f $TMP_PATH/pgbackrest.*.conf
 
-info "Setting chown to postgres"
-sudo chown postgres:postgres /usr/local/bin/cbob*
+info "Setting chown to postgres on executables"
+chown postgres:postgres /usr/local/bin/cbob*
 
 info "Disabling postgresql service from start on boot"
-sudo systemctl disable postgresql
+systemctl disable postgresql
 
-info "Starting postgresql clusters"
-cbob_postgres_start
+info "Finished Successfully!"
